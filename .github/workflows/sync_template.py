b35ce41292c0a52aa23edba6336d000b1200bf32
rplacesuperstonk <102842881+rplacesuperstonk@users.noreply.github.com>
2022-04-04 01:42:31 -0300

    Update sync_template.py

--- .github/workflows/sync_template.py
+++ .github/workflows/sync_template.py
@@ -3,13 +3,13 @@ import requests
 from io import BytesIO
 
 tl = (773 * 3, 735  * 3) # top left corner main image
-tl_taskbar = (933 * 3, 1972 * 3) # top left corner main image
+tl_taskbar = (930 * 3, 1971 * 3) # top left corner main image
 map_dim = (2000, 2000)
 
 img = Image.open("reference.png")
 img = img.resize((img.size[0] * 3, img.size[1] * 3), Image.NEAREST)
 
-img_taskbar = Image.open("taskbar.png")
+img_taskbar = Image.open("taskbar_temp.png")
 img_taskbar = img_taskbar.resize((img_taskbar.size[0] * 3, img_taskbar.size[1] * 3), Image.NEAREST)
 
 mask_url = "https://cdn.discordapp.com/attachments/959177054149025802/960340522583609404/new_mask.png"
@@ -25,7 +25,8 @@ placestart = Image.open(BytesIO(response.content))
 final_img = Image.new('RGBA', (6000, 6000))
 unmasked_img = Image.new('RGBA', (6000, 6000))
 unmasked_img.paste(img, tl)
-#unmasked_img.paste(img_taskbar, tl_taskbar)
+unmasked_img.paste(img_taskbar, tl_taskbar)
 unmasked_img.paste(placestart, (0,0), placestart)
+unmasked_img.paste(img_taskbar, tl_taskbar)
 final_img = Image.composite(final_img, unmasked_img, mask)
 final_img.save("superstonk_overlay.png")
