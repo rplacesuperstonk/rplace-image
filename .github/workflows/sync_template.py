55d776fcd5d45a9b41832f32431c0164caa0690a
rplacesuperstonk <102842881+rplacesuperstonk@users.noreply.github.com>
2022-04-03 21:42:49 -0300

    Update sync_template.py

--- .github/workflows/sync_template.py
+++ .github/workflows/sync_template.py
@@ -11,11 +11,15 @@ mask_i = Image.open(BytesIO(response.content))
 mask = Image.new("1", (3000, 3000), 0)
 mask.paste(mask_i)
 
-tl = (773 * 3, 735  * 3) # top left corner
+tl = (773 * 3, 735  * 3) # top left corner main image
+tl_taskbar = (702 * 3, 1972  * 3) # top left corner main image
+
+img_taskbar = Image.open("taskbar.png")
+img_taskbar = img_taskbar.resize((img_taskbar.size[0] * 3, img_taskbar.size[1] * 3), Image.NEAREST)
 
 final_img = Image.new('RGBA', (3000, 3000))
 unmasked_img = Image.new('RGBA', (3000, 3000))
 unmasked_img.paste(img, tl)
+unmasked_img.paste(img_taskbar, tl_taskbar)
 final_img = Image.composite(final_img, unmasked_img, mask)
 final_img.save("superstonk_overlay.png")
-
